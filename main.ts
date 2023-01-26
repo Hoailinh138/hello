03744f110e9e68691b7554d3db02412b2f061c2f
Hoailinh138 <123626540+Hoailinh138@users.noreply.github.com>
2023-01-26 15:31:39 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+Mbit_IR.onPressEvent(RemoteButton.Power, function () {
+    basic.showIcon(IconNames.SmallHeart)
+})
+Mbit_IR.onPressEvent(RemoteButton.Light, function () {
+    basic.showIcon(IconNames.Heart)
+})
+basic.forever(function () {
+	
+})
